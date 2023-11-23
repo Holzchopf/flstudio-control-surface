c2734fb359f7e01bc538156eb4dd43f0e787199a
Olivier Stuker <oli4stuker@hotmail.com>
2023-11-23 15:45:12 +0100

    FIXED index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from "./control-surface"
+export * from "./control-surface-control"
 export * from "./control-surface-event"
+export * from "./control-surface-event-group"
 export * from "./control-surface-event-type"
+export * from "./control-surface-options"
