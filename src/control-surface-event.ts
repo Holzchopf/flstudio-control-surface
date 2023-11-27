98a7f31eda91a58a743e7786dc341e46fa537e3e
Olivier Stuker <oli4stuker@hotmail.com>
2023-11-27 16:30:22 +0100

    ADDED event type specific classes

--- src/control-surface-event.ts
+++ src/control-surface-event.ts
@@ -84,6 +84,96 @@ export class ControlSurfaceStringEvent extends ControlSurfaceEvent<string> {
   }
 }
 
+export class ControlSurfaceSettingsEvent extends ControlSurfaceBinaryEvent {
+  
+  constructor(type: number) {
+    super(type)
+    this.setBinary(new ArrayBuffer(64))
+  }
+
+  getSkip(): number {
+    return this.view!.getUint32(0, true)
+  }
+  setSkip(value: number) {
+    this.view!.setUint32(0, value, true)
+  }
+
+  private getFlag(bit: number): boolean {
+    let flags = this.view!.getUint32(4, true)
+    return !!((flags >> bit) & 0x01)
+  }
+  private setFlag(bit: number, value: boolean) {
+    let flags = this.view!.getUint32(4, true)
+    flags |= (+value << bit)
+    this.view!.setUint32(4, flags, true)
+  }
+
+  getEditFlag(): boolean {
+    return this.getFlag(0)
+  }
+  setEditFlag(value: boolean) {
+    this.setFlag(0, value)
+  }
+
+  getHideButtonsFlag(): boolean {
+    return this.getFlag(1)
+  }
+  setHideButtonsFlag(value: boolean) {
+    this.setFlag(1, value)
+  }
+
+  getHideLabelsFlag(): boolean {
+    return this.getFlag(2)
+  }
+  setHideLabelsFlag(value: boolean) {
+    this.setFlag(2, value)
+  }
+
+  getGridSize(): number {
+    return this.view!.getUint32(8, true)
+  }
+  setGridSize(value: number) {
+    this.view!.setUint32(8, value, true)
+  }
+}
+
+export class ControlSurfaceDimensionsEvent extends ControlSurfaceBinaryEvent {
+  
+  constructor(type: number) {
+    super(type)
+    this.setBinary(new ArrayBuffer(8))
+  }
+
+  getWidth(): number {
+    return this.view!.getUint32(0, true)
+  }
+  setWidth(value: number) {
+    this.view!.setUint32(0, value, true)
+  }
+
+  getHeight(): number {
+    return this.view!.getUint32(4, true)
+  }
+  setHeight(value: number) {
+    this.view!.setUint32(4, value, true)
+  }
+}
+
+export class ControlSurfaceStartControlEvent extends ControlSurfaceBinaryEvent {
+  
+  constructor(type: number) {
+    super(type)
+    this.setBinary(new ArrayBuffer(32))
+  }
+
+  getType(): number {
+    return this.view!.getUint32(0, true)
+  }
+  setType(value: number) {
+    this.view!.setUint32(0, value, true)
+  }
+}
+
 /**
  * Describes how a control is exposed. Enabled controls will have at least one of these events.
  */
@@ -131,5 +221,41 @@ export class ControlSurfaceEnableControlEvent extends ControlSurfaceBinaryEvent 
    */
   setIndex(value: number) {
     this.view!.setUint32(0, value, true)
+  }
+}
+
+export class ControlSurfaceControlDimensionsEvent extends ControlSurfaceBinaryEvent {
+  
+  constructor(type: number) {
+    super(type)
+    this.setBinary(new ArrayBuffer(16))
+  }
+
+  getX(): number {
+    return this.view!.getFloat32(0, true)
+  }
+  setX(value: number) {
+    this.view!.setFloat32(0, value, true)
+  }
+
+  getY(): number {
+    return this.view!.getFloat32(4, true)
+  }
+  setY(value: number) {
+    this.view!.setFloat32(4, value, true)
+  }
+
+  getWidth(): number {
+    return this.view!.getFloat32(8, true)
+  }
+  setWidth(value: number) {
+    this.view!.setFloat32(8, value, true)
+  }
+
+  getHeight(): number {
+    return this.view!.getFloat32(12, true)
+  }
+  setHeight(value: number) {
+    this.view!.setFloat32(12, value, true)
   }
 }
